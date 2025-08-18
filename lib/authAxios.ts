d05cbfda89b975c742c0694e4479b6c0af1b165e
datnfrontend <mainam531@gmail.com>
2025-08-18 15:17:17 +0700

    fix

--- lib/authAxios.ts
+++ lib/authAxios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const authAxios = axios.create({
-  baseURL: 'http://localhost:8000/api/v1/auth', // Đảm bảo là đúng URL của backend
+  // baseURL: 'http://localhost:8000/api/v1/auth',
+  baseURL: 'https://api-gateway-egomall.io.vn/api/v1/auth',
   withCredentials: true, // Cho phép gửi cookie (nếu cần)
 });
 
